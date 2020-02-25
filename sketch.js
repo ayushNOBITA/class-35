0848ca0711db1db9f7aeef3ebc22e8d1b3612068
ayushNOBITA <54540853+ayushNOBITA@users.noreply.github.com>
2020-02-25 19:00:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var gameState=0
+var playerCount;
+var form,player,game;
+var database,canvas
+
+function setup(){
+    database=firebase.database();
+    canvas=createCanvas(500,500);
+   
+}
+
+function draw(){
+   
+}
+
